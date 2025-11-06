67c1c8ddfd293bca851be7c6cb28aa1f70cff10d
MisaqArtist7 <misaqbabakhani777@gmail.com>
2025-11-06 12:00:43 +0330

    fix bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
 import "./globals.css";
 import GlobalLoader from "./global-loader";
+
+import type { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "مستر وحید | رنت ویلا و ماشین",
+  description: ""
+}
 export default function RootLayout({ children, } : Readonly<{children : React.ReactNode }>) {
   return (
     <html lang="fa" dir="rtl">
