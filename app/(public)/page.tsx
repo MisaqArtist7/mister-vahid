f17151bbbc1090bbc4a0f690b548c389edaffb96
MisaqArtist7 <misaqbabakhani777@gmail.com>
2025-11-06 06:25:13 +0330

    update

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,26 +1,3 @@
-import getPosts from "@/src/services/posts.service";
-import { Suspense } from "react";
-
-interface Post {
-  id: number;
-  title: string;
-  body: string;
-}
-
-async function Articles() {
-  const posts: Post[] = (await getPosts()) ?? []; // nunlish
-
-  return (
-    <>
-      {posts.map(post => (
-        <div key={post.id} className="bg-white shadow p-3 rounded my-3">
-          <h1>{post.id}: {post.title}</h1>
-        </div>
-      ))}
-    </>
-  );
-}
-
 export default function HomePage() {
   return (
     <div className="container">
